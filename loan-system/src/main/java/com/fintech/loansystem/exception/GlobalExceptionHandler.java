70d78503967b58406201ccd45923e9259d0463f4
forough <forough_majidi@yahoo.com>
2025-02-03 12:39:38 +0330

    Add exception

--- loan-system/src/main/java/com/fintech/loansystem/exception/GlobalExceptionHandler.java
+++ loan-system/src/main/java/com/fintech/loansystem/exception/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package com.fintech.loansystem.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Map<String, String>> handleAuthenticationException(AuthenticationException ex) {
+    public ResponseEntity<Object> handleAuthenticationException(AuthenticationException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(Map.of(
                         "error", "Invalid credentials, please try again",
@@ -21,21 +28,38 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGeneralException(AuthenticationException ex) {
+    public ResponseEntity<Object> handleGeneralException(AuthenticationException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("An error occurred:", ex.getMessage()));
     }
 
     @ExceptionHandler(LoanTypeNotSupportedException.class)
-    public ResponseEntity<Map<String, String>> handleLoanTypeNotSupportedException(LoanTypeNotSupportedException ex) {
+    public ResponseEntity<Object> handleLoanTypeNotSupportedException(LoanTypeNotSupportedException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("error", ex.getMessage()));
     }
 
     @ExceptionHandler(LoanNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleLoanNotFoundException(LoanNotFoundException ex) {
+    public ResponseEntity<Object> handleLoanNotFoundException(LoanNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("error", ex.getMessage()));
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatus status,
+                                                               WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        allErrors.forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
