953d0f1680c7df135b27b9118169b1b858e2ce68
forough <forough_majidi@yahoo.com>
2025-02-03 11:44:45 +0330

    create userAuthentication

---
+++ loan-system/src/main/java/com/fintech/loansystem/security/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.fintech.loansystem.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String secretKey;
+
+    private final long expirationTime;
+
+    public JwtUtil(@Value("${jwt.secret}") String secretKey,
+                   @Value("${jwt.expiration}") long expirationTime) {
+        this.secretKey = secretKey;
+        this.expirationTime = expirationTime;
+    }
+
+
+    public String generateToken(String userName) {
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.ES256, secretKey)
+                .compact();
+    }
+
+    public String extractUserName(String token) {
+        return extractClaims(token).getSubject();
+
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String userName) {
+        return (userName.equals(extractUserName(token)) && !isTokenExpired(token));
+    }
+
+
+}
+
