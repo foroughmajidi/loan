aec861d8bce99dcb683e03fc7bd30768ebb0c457
forough <forough_majidi@yahoo.com>
2025-02-03 07:08:39 +0330

    create LoanResponseDto

---
+++ loan-system/src/main/java/com/fintech/loansystem/dto/LoanResponseDto.java
@@ -0,0 +1,17 @@
+package com.fintech.loansystem.dto;
+
+import com.fintech.loansystem.enums.LoanType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+
+public class LoanResponseDto {
+    private Long id;
+    private LoanType loanType;
+    private BigDecimal amount;
+    private BigDecimal interest;
+    private LocalDateTime createdAt;
+}
