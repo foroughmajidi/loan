673121226024fe65d65809ac643180e589d05031
forough <forough_majidi@yahoo.com>
2025-02-03 11:43:41 +0330

    create userAuthentication

---
+++ loan-system/src/main/java/com/fintech/loansystem/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.fintech.loansystem.service;
+
+import com.fintech.loansystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.fintech.loansystem.model.User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("User")
+                .build();
+
+
+    }
+
+}
